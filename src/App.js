e435b3989a37d0616a63ce587512f6ca388861c2
radhe212221 <radhe212221@gmail.com>
2022-05-25 22:16:40 +0530

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import "./style.css";
+import React, { useRef } from 'react';
+import './style.css';
 
 export default function App() {
+  const ref = useRef();
+  const handleclick = () => {
+    ref.current.innerHTML -=1
+  };
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <button onClick={handleclick}>click</button>
+      <h1 ref={ref}></h1>
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    </>
   );
 }
